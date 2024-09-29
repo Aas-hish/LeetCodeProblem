be857475af549884d9f20545858e8c447ab2ed8d
Aashish <rebelraaz2@gmail.com>
2024-09-29 15:23:41 +0530

    final

---
+++ src/ValidParantheses.java
@@ -0,0 +1,15 @@
+public class ValidParantheses {
+        public static boolean isValid(String s) {
+            int length;
+            do{
+                length= s.length();
+                s=s.replace("()","").replace("[]","").replace("{}","");
+            }while(length != s.length());
+            return s.isEmpty();
+        }
+
+        public static void main(String[] args) {
+            System.out.println(isValid("()[]{}"));
+        }
+    }
+
